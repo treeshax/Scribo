8a0c9667d819cc12cbbbe227decdc04c177bdadd
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-04-21 12:34:00 +0530

    feat: add back navigation button to ScriboAI page and fix routing implementation

--- src/components/ScriboAI.jsx
+++ src/components/ScriboAI.jsx
@@ -1,19 +1,26 @@
 import Spline from '@splinetool/react-spline';
 import Navbar from './ui/Navbar';
 import './ScriboAI.css';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { ArrowLeft } from 'lucide-react';
 
 // We used Spline here for this 3D AI animation
 export default function ScriboAI() {
+  const navigate = useNavigate();
   return (
     <div className="scribo-ai-page">
       <Navbar />
+      <button onClick={() => navigate('/')} className='home_btn'>
+        <ArrowLeft size={16} /> 
+        Back to home
+      </button>
       <Spline
         scene="https://prod.spline.design/dZrvOwxtx7a-OGKE/scene.splinecode" 
       />
       
+      
       <div className="ai-content">
-        <button className="get-started-btn" onClick={() => Navigate('/')}>
+        <button className="get-started-btn" onClick={() => navigate('/')}>
           Get Started
         </button>
       </div>
